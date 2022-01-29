3b9003292286fa573acb58b1d75ef76ae92ecb2d
alkihuri <alkihuri@gmail.com>
2022-01-29 11:37:59 +0200

    Cursor Mode Settings

--- FinalProject.Untiy/Assets/Scripts/GameCore/Player/Movement/Movement.cs
+++ FinalProject.Untiy/Assets/Scripts/GameCore/Player/Movement/Movement.cs
@@ -10,7 +10,7 @@ public class Movement : MonoBehaviour
     [SerializeField] GameObject _camera;
     // Start is called before the first frame update
     void Start()
-    {
+    { 
         _cc = GetComponent<CharacterController>();
         _camera = GetComponentInChildren<Camera>().gameObject;
     }
@@ -18,12 +18,26 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        DoMovement(); 
+        CursorModeSettings(); 
+    }
+
+    private void DoMovement()
+    {
         float v = Input.GetAxis("Vertical");
-        float h = Input.GetAxis("Horizontal"); 
+        float h = Input.GetAxis("Horizontal");
         Vector3 directionOfMovement = (transform.forward * v + transform.right * h) * _speed;
         _cc.SimpleMove(directionOfMovement);
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
         _camera.transform.Rotate(0, mouseX, 0);
+    }
+
+    private static void CursorModeSettings()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Cursor.lockState = CursorLockMode.None;
+        if (Input.GetMouseButtonDown(0))
+            Cursor.lockState = CursorLockMode.Locked;
     }
 }
