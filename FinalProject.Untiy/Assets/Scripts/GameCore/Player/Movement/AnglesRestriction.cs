0e062e0c50849ef83e7e802038329612f0101fdb
alkihuri <alkihuri@gmail.com>
2022-02-05 11:47:56 +0200

    Update AnglesRestriction.cs

--- FinalProject.Untiy/Assets/Scripts/GameCore/Player/Movement/AnglesRestriction.cs
+++ FinalProject.Untiy/Assets/Scripts/GameCore/Player/Movement/AnglesRestriction.cs
@@ -7,15 +7,18 @@ public class AnglesRestriction : MonoBehaviour
     [SerializeField] GameObject _camera;
 
     [SerializeField, Range(0, 45)] float _xRestriction;
-    [SerializeField, Range(0, 45)] float _currentXCameraAngle;
+    [SerializeField, Range(-45, 45)] float _currentXCameraAngle;
+    [SerializeField, Range(-45, 45)] float _currentClampedXCameraAngle;
     [SerializeField] bool _isOk;
 
     public bool IsOk { get { return _isOk; } }
 
     // Update is called once per frame
     void Update()
-    { 
-        _currentXCameraAngle = Mathf.Abs(_camera.transform.localEulerAngles.x);
+    {
+        _currentXCameraAngle = _camera.transform.localEulerAngles.x ;
+        _currentClampedXCameraAngle = Mathf.Clamp(_currentXCameraAngle, -_xRestriction, _xRestriction);
         _isOk = _currentXCameraAngle < _xRestriction;
+        
     }
 }
