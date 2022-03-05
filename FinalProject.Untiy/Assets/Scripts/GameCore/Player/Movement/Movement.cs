859e8e42fdb3f2e0ac558dd345d4247bb73941ea
alkihuri <alkihuri@gmail.com>
2022-03-05 11:34:38 +0200

    JoystickAdded

--- FinalProject.Untiy/Assets/Scripts/GameCore/Player/Movement/Movement.cs
+++ FinalProject.Untiy/Assets/Scripts/GameCore/Player/Movement/Movement.cs
@@ -7,20 +7,22 @@ public class Movement : MonoBehaviour
     public CharacterController controller;
     public float speed = 12f;
     Vector3 velocity;
-    public float gravity = -9.81f; 
+    public float gravity = -9.81f;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     bool isGrounded;
+
+    [SerializeField] FloatingJoystick _joystick;
     private void Update()
     {
         isGrounded = controller.isGrounded;
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal") + _joystick.Horizontal;
+        float z = Input.GetAxis("Vertical") + _joystick.Vertical;
 
         Vector3 move = transform.right * x + transform.forward * z;
 
