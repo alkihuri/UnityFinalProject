1d81787a04e9ca34ac84d1ab1d4360e923cbf47d
alkihuri <alkihuri@gmail.com>
2022-03-19 11:21:08 +0200

    Cinemachine Intro

--- FinalProject.Untiy/Assets/Scripts/UI/SceneController.cs
+++ FinalProject.Untiy/Assets/Scripts/UI/SceneController.cs
@@ -5,6 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+
+    [SerializeField, Range(1, 10)] int _introSceneDuration = 10;
+    [SerializeField] bool _isIntroScene;
+
+    public void LoadGameSceneWithDelay(int seconds)
+    {
+        Invoke("LoadGameScene", seconds);
+    }
+
+    private void Start()
+    {
+        if(_isIntroScene)
+            LoadGameSceneWithDelay(_introSceneDuration);
+    }
+
+    public void LoadGameScene()
+    {
+        LoadScene("Game");
+    }
      public void LoadScene(string name)
     {
         SceneManager.LoadScene(name);
